1d18a34a550de7643a74a6770dbd1cd49762b47f
harmony.is.valued@gmail.com <53105202+zetzetn@users.noreply.github.com>
2020-07-31 22:19:18 +0900

    ログイン機能の導入​

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,10 @@ module WeightManagementApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
+    # 日本語に変更
+    config.i18n.default_locale = :ja
+    # タイムゾーンも変更
+    config.time_zone = 'Asia/Tokyo'
     config.generators.system_tests = nil
   end
 end
